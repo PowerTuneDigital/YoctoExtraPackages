e2347cc6ddc24e650dbbd0a3d6add5620fc2c80e
root <info@powertunedigital.com>
2023-07-16 00:21:48 +0000

    final script and binaries

--- installopenssl.sh
+++ installopenssl.sh
@@ -10,9 +10,20 @@ tar -xzf "$TARBALL_NAME"
 # Step 2: Copy files to the correct folder structure
 cp -R "openssl-${OPENSSL_VERSION}/"* "$INSTALL_DIR/"
 
-# Step 3: Set necessary path variables
-export LD_LIBRARY_PATH="$INSTALL_DIR/lib:$LD_LIBRARY_PATH"
-export PATH="$INSTALL_DIR/bin:$PATH"
+# Step 3: Set necessary path variables if not already present in ~/.bashrc
+BASHRC="/home/root/.bashrc"
 
-# Step 4: Clean up
+if ! grep -q "export PATH=\"$INSTALL_DIR/bin:\$PATH\"" "$BASHRC"; then
+  echo "export PATH=\"$INSTALL_DIR/bin:\$PATH\"" >> "$BASHRC"
+fi
+
+if ! grep -q "export LD_LIBRARY_PATH=\"$INSTALL_DIR/lib:\$LD_LIBRARY_PATH\"" "$BASHRC"; then
+  echo "export LD_LIBRARY_PATH=\"$INSTALL_DIR/lib:\$LD_LIBRARY_PATH\"" >> "$BASHRC"
+fi
+
+# Step 4: Source the ~/.bashrc file to apply changes immediately
+source "$BASHRC"
+
+# Step 5: Clean up
 rm -rf "openssl-${OPENSSL_VERSION}"
+
